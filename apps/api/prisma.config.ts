3bde88394da7af2d0c148796b4c178f35c7caac7
ThinhTran <thinhtran9797yb@gmail.com>
2026-01-29 00:56:20 +0700

    config prisma in be

---
+++ apps/api/prisma.config.ts
@@ -0,0 +1,15 @@
+// apps/api/prisma.config.ts
+import 'dotenv/config';
+import { defineConfig } from '@prisma/config';
+import path from 'path';
+
+export default defineConfig({
+  schema: path.resolve(__dirname, './prisma/schema.prisma'),
+  migrations: {
+    path: path.resolve(__dirname, './prisma/migrations'),
+  },
+  engine: 'classic',
+  datasource: {
+    url: process.env.DATABASE_URL as string,
+  },
+});
